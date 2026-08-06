62fd25a5778b9253811771a92e25edb298bb3ac6
taha <jokerdz380@gmail.com>
2026-08-06 23:56:33 +0100

    dlq setup

--- consumer/src/kafka/consumers/DLQ.transaction.ts
+++ consumer/src/kafka/consumers/DLQ.transaction.ts
@@ -2,12 +2,14 @@ import { Injectable, OnModuleInit } from "@nestjs/common"
 import { ConsumerSvc } from "../kafka.service"
 import { PrometheusService } from "@/prometheus/prometheus.service";
 import { Loggsvc } from "@/user/Logger.svc";
+import { UserService } from "@/user/user.service";
 
 @Injectable()
 export class TransactionDLQConsumer implements OnModuleInit {
   constructor(private readonly kafka: ConsumerSvc ,
         private readonly metrics: PrometheusService,
         private readonly logger :Loggsvc,
+        private readonly userService:UserService
 
   ) { }
   async onModuleInit() {
@@ -18,12 +20,21 @@ export class TransactionDLQConsumer implements OnModuleInit {
     }, {
       eachMessage: async ({ message }) => {
             this.metrics.increment();
-        const payload = JSON.parse(message.value?.toString() ?? '{}');
-        try {
-          this.logger.log('the payload✅✅✅✅✅✅', payload)
-        } catch (e) {
-          this.logger.log('FAILED', e, `${payload}`,)
-        }
+        // const payload = JSON.parse(message.value?.toString() ?? '{}');
+        // try {
+        //   this.logger.log('the payload✅✅✅✅✅✅', payload)
+        // } catch (e) {
+        //   this.logger.log('FAILED', e, `${payload}`,)
+        // }
+          const dto = JSON.parse(message.value?.toString() ?? '{}');
+
+          try {
+            await this.userService.process(dto);
+          }
+          catch (err) {
+            this.logger.log("Error LOGGED❌❌❌❌❌❌❌", err)
+            throw new Error('Retry not works')
+          }
       }
     })
   }
